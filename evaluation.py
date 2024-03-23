a3eb77dae9cfe8262395b92a54c11394d38ed537
Vico Pratama <135086306+guguboo@users.noreply.github.com>
2024-03-23 10:36:41 +0700

    check combinasi semua band

--- evaluation.py
+++ evaluation.py
@@ -11,53 +11,125 @@ import os
 import pandas as pd
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
 import matplotlib.pyplot as plt
+from itertools import combinations
 import seaborn as sns
 
-#%% read excel dan masukan ke variabel
-
 script_directory = os.path.dirname(os.path.abspath(__file__))
-df = pd.read_excel(script_directory + "/prediction_results.xlsx")
+#%% read excel dan masukan ke variabel (CONTOH)
 
-#%% contoh arraynya
+df = pd.read_excel(script_directory + "/prediction_result/prediction_result_B1_B2_B3.xlsx")
 
 prediction_array = df['Predicted']
 true_array = df['Actual']
-classes = set(true_array.unique())
+evaluation_function(prediction_array, true_array, [""])
 
-#%% function buat matrix
-def evaluation_function(prediction_array, truth_array):
-    # Compute confusion matrix
-    cm = confusion_matrix(truth_array, prediction_array)
+#%% function buat matrix (andrea version)
+
+# def evaluation_function(prediction_array, truth_array, bands):
     
-    # Plot confusion matrix
-    plt.figure(figsize=(8, 6))
-    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", 
-                xticklabels=classes, yticklabels=classes)
-    plt.xlabel('Predicted')
-    plt.ylabel('True')
-    plt.title('Confusion Matrix')
-    plt.show()
+#     global f1_air_max, f1_bangunan_max, f1_area_hijau_max
+#     global band_air, band_bangunan, band_area_hijau
+#     # Compute confusion matrix
+#     cm = confusion_matrix(truth_array, prediction_array)
+#     classes = set(truth_array.unique())
     
-    # Compute metrics
-    accuracy = accuracy_score(truth_array, prediction_array)
-    precision = precision_score(truth_array, prediction_array, average=None)
-    recall = recall_score(truth_array, prediction_array, average=None)
+#     # Plot confusion matrix
+#     plt.figure(figsize=(8, 6))
+#     sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", 
+#                 xticklabels=classes, yticklabels=classes)
+#     plt.xlabel('Predicted')
+#     plt.ylabel('True')
+#     plt.title('Confusion Matrix')
+#     plt.show()
+    
+#     # Compute metrics
+#     accuracy = accuracy_score(truth_array, prediction_array)
+#     precision = precision_score(truth_array, prediction_array, average=None)
+#     recall = recall_score(truth_array, prediction_array, average=None)
+#     f1 = f1_score(truth_array, prediction_array, average=None)
+    
+#     # Print metrics
+#     print("Accuracy: {:.2f}%".format(accuracy * 100))
+#     print("Precision")
+#     for i, cls in enumerate(classes):
+#         print("Class", cls, ":", "{:.2f}%".format(precision[i] * 100))
+#     print("Recall")
+#     for i, cls in enumerate(classes):  
+#         print("Class", cls, ":", "{:.2f}%".format(recall[i] * 100))
+#     print("F1 score")
+#     for i, cls in enumerate(classes):
+#         print("Class", cls, ":", "{:.2f}%".format(f1[i] * 100))
+
+#     f1_weighted = f1_score(truth_array, prediction_array, average='weighted')
+#     print("F1 Score (weighted): {:.2f}%".format(f1_weighted * 100))
+#     print()
+    
+    
+#%% best variables
+
+band_air = []
+band_bangunan = []
+band_area_hijau = []
+band_weighted = []
+
+f1_air_max = 0
+f1_bangunan_max = 0
+f1_area_hijau_max = 0
+best_weighted = 0
+
+#%% function buat matrix (VICO VERSION)
+
+def evaluation_function(prediction_array, truth_array, bands):
+    
+    global f1_air_max, f1_bangunan_max, f1_area_hijau_max, best_weighted
+    global band_air, band_bangunan, band_area_hijau, band_weighted
+
+    classes = set(truth_array.unique())
+    
     f1 = f1_score(truth_array, prediction_array, average=None)
     
-    # Print metrics
-    print("Accuracy: {:.2f}%".format(accuracy * 100))
-    print("Precision")
     for i, cls in enumerate(classes):
-        print("Class", cls, ":", "{:.2f}%".format(precision[i] * 100))
-    print("Recall")
-    for i, cls in enumerate(classes):  
-        print("Class", cls, ":", "{:.2f}%".format(recall[i] * 100))
-    print("F1 score")
-    for i, cls in enumerate(classes):
-        print("Class", cls, ":", "{:.2f}%".format(f1[i] * 100))
-
+        if(cls == "air"):
+            if(f1[i] > f1_air_max):
+                f1_air_max = f1[i]
+                band_air = bands
+        elif(cls == "bangunan"):
+            if(f1[i] > f1_bangunan_max):
+                f1_bangunan_max = f1[i]
+                band_bangunan = bands
+        else:
+            if(f1[i] > f1_area_hijau_max):
+                f1_area_hijau_max = f1[i]
+                band_area_hijau = bands
+        
     f1_weighted = f1_score(truth_array, prediction_array, average='weighted')
-    print("F1 Score (weighted): {:.2f}%".format(f1_weighted * 100))
+    if(f1_weighted > best_weighted):
+        best_weighted = f1_weighted
+        band_weighted = bands
+        
+#%% Check untuk setiap kombinasi
 
-evaluation_function(prediction_array, true_array)
-#%%
+X = ['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B11', 'B12']
+    
+for i in range (1, 11):
+    comb = combinations(X, i)
+     
+    for i in list(comb): 
+        selected = list(i)
+        nama_file = "prediction_result"
+        
+        for b in selected:
+            nama_file += "_" + b
+            
+        df = pd.read_excel(script_directory + "/prediction_result/combination(all_bands)/" + nama_file + ".xlsx")
+    
+        prediction_array = df['Predicted']
+        true_array = df['Actual']
+            
+        evaluation_function(prediction_array, true_array, selected)
+    
+print(f"{band_air} is the best for air, with f1 score: {f1_air_max * 100:.2f}%")
+print(f"{band_bangunan} is the best for bangunan, with f1 score: {f1_bangunan_max * 100:.2f}%")
+print(f"{band_area_hijau} is the best for area_hijau, with f1 score: {f1_area_hijau_max * 100:.2f}%")
+print(f"{band_weighted} is the best for overall, with f1 score: {best_weighted * 100:.2f}%")
+
--- training.py
+++ training.py
@@ -11,6 +11,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, accuracy_score
+from itertools import combinations
+
 
 script_directory = os.path.dirname(os.path.abspath(__file__))
 
@@ -25,6 +27,7 @@ data = pd.read_excel(script_directory + '/output_labelling' + '/dataset_satelit_
 
 
 X = data[['B2', 'B4']]  # Features: Spectral bands B2, B3, B4
+# X = data[['B4', 'B6', 'B7', 'B8', 'B11', 'B12']]  
 
 y = data['jenis_lahan']        # Target label: Land cover types
 
@@ -46,40 +49,48 @@ results_df = pd.DataFrame({'Actual': y_test.values, 'Predicted': y_pred})
 # Export the results DataFrame to an Excel file
 results_df.to_excel(script_directory + '/prediction_results.xlsx', index=False)
 
+
+
+#%%
+
+def train(x, y, bands):
+    
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+    rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+
+    rf_classifier.fit(X_train, y_train)
+
+    y_pred = rf_classifier.predict(X_test)
+
+    results_df = pd.DataFrame({'Actual': y_test.values, 'Predicted': y_pred})
+
+    nama_file = "prediction_result"
+    
+    for b in bands:
+        nama_file += "_" + b
+        
+    results_df.to_excel(script_directory + '/prediction_result/combination(all_bands)/' + nama_file + '.xlsx', index=False)
 #%% KODE TRAINING COBA 20m
 
 # Load the data from the Excel file
 
 data = pd.read_excel(script_directory + '/output_labelling' + '/dataset_satelit_latihan_20m_1.xlsx')
 
-# Split the data into features (spectral bands) and target label
-
 #data kalo baseline yg ini
-# X = data[['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B11', 'B12']]
+X = ['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B11', 'B12']
+y = data['jenis_lahan']
+    
+for i in range (1, 11):
+    comb = combinations(X, i) 
+     
+    for i in list(comb): 
+        selected = list(i)
+        x = data[selected]
+        train(x, y, selected)
+        
+print("done creating prediction files...")
 
 
-#data setelah feature engineering
-X = data[['B4', 'B6', 'B7', 'B8', 'B11', 'B12']]  
 
-
-
-y = data['jenis_lahan']        # Target label: Land cover types
-
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Initialize the Random Forest Classifier
-rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
-
-# Train the classifier on the training data
-rf_classifier.fit(X_train, y_train)
-
-# Predict land cover types for the test data
-y_pred = rf_classifier.predict(X_test)
-
-# Create a DataFrame to store predicted and actual labels
-results_df = pd.DataFrame({'Actual': y_test.values, 'Predicted': y_pred})
-
-# Export the results DataFrame to an Excel file
-results_df.to_excel(script_directory + '/prediction_results.xlsx', index=False)
 
