a35a6442c804cc95111d8e5e90eca3e7cf20f1b6
Andrea Miranti Artura <135795514+dreyyeahh@users.noreply.github.com>
2024-11-08 08:38:41 +0700

    maps+pilihan DTA

--- ProDS2/GUI/app.py
+++ ProDS2/GUI/app.py
@@ -10,18 +10,24 @@ def index():
         shapefile_path = 'mygeodata/doc-polygon.shp'
         gdf = gpd.read_file(shapefile_path)
 
+        if gdf.crs != 'EPSG:4326':
+            gdf = gdf.to_crs('EPSG:4326')
+
         # Get the bounds of the shapefile
         minx, miny, maxx, maxy = gdf.total_bounds
-        center = [(miny + maxy) / 2, (minx + maxx) / 2]
+        center = list(gdf.geometry.centroid.unary_union.centroid.coords[0])[::-1]
 
         # Convert GeoDataFrame to GeoJSON
         geojson_data = gdf.to_json()
 
         print(f'Center: {center}, Bounds: {[minx, miny, maxx, maxy]}')  # Debugging output
         print(gdf.head())            # Check the first few rows of the data
+        names = gdf['name'].tolist()
+        print()
+        print(names)
 
         # Pass the center, bounds, and GeoJSON data to the frontend
-        return render_template('index.html', center=center, bounds=[minx, miny, maxx, maxy], geojson_data=geojson_data)
+        return render_template('index.html', center=center, bounds=[minx, miny, maxx, maxy], geojson_data=geojson_data, dta=names)
     except Exception as e:
         print(f"Error: {e}")
         return "An error occurred. Please check the console for details."
